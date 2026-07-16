bb7eaba47d18661c3466436e6ac188f93d0fd84c
Mohit Telang <159064386+Mohit2656@users.noreply.github.com>
2026-07-16 22:40:07 +0530

    Time: 27 ms (18.45%), Space: 111.2 MB (5.11%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,15 @@
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-     int sum=0;
-     boolean x=true;
-     for(int i=0;i<nums.length;i++){
-        int j=i+1;
-        sum=nums[i]+nums[i+1];
-        if(i-1>=0){
-            sum-=nums[i-1];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
         }
-   
-        if(sum<k){
-            x=false;
-        }
-     }
-     return x;
+
+        return false;
     }
 }
